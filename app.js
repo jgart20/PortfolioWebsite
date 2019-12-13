de93d7a669823900f5988e4d8219cfc293a4fe0a
Jeremy Gart <jgart20@jgart20.local>
2019-12-13 09:09:53 -0700

    General node.js website code

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+var favicon = require('serve-favicon');
+const path = require('path');
+
+//init app
+const app = express();
+
+app.use(favicon(path.join(__dirname, 'Public/startbootstrap-resume-gh-pages', 'favicon.ico')));
+
+//load view engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+//Set Public Folder
+app.use(express.static(path.join(__dirname, 'Public')))
+
+//Home route
+app.get('/', function(req, res){
+    let articles = [
+        {
+            id:1,
+            title:'Welcome to my website! \n feel free to navigate around, and reach out to me if you have any questions!',
+            author:'Jeremy Gart',
+            body:'This is a sample article'
+        }
+    ];
+    res.render('index', {
+        title: 'Jeremy Gart',
+        articles: articles
+    });
+});
+
+//add Route
+app.get('/articles/add', function(req, res){
+    res.render('add_article', {
+        title: 'Add Article'
+    });
+})
+
+//start server
+app.listen(3000, function(){
+    console.log('server started on port 3000');
+});
